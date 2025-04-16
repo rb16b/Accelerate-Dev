127a2b4cff2de292c2046056823c7aa02d3959d4
wserafin <wserafin@gmail.com>
2025-04-16 15:12:15 -0400

    fixed run-Load URL

--- dev-user-objects/run-load.sh
+++ dev-user-objects/run-load.sh
@@ -1,7 +1,7 @@
 
 while [[ true ]]; do
    curl -X 'GET' \
-  'http://rest-fights-dev-user1.apps.cluster-lzxlf.lzxlf.sandbox1417.opentlc.com/api/fights/hello' \
+  'http://RELPLACEMEm/api/fights/hello' \
   -H 'accept: text/plain'
 
 done
